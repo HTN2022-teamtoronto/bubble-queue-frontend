82bfd198f0607fecdeebd5e7bfcc5d43c04908fe
Xiao215 <xiaozhang20030215@gmail.com>
2022-09-17 13:59:05 -0400

    tempalte

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '@mui/material/Slider';
+
+function App(){
+  return (
+    <div className="font-raleway" >
+      
+    </div>
+  );
+}
+
+export default App;
